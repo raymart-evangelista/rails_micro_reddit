15dc14f2867509537754396d052520c0c1aebd68
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-16 19:52:46 -0700

    add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :username, :password, presence: true
+  validates :username, length: { minimum: 3}, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, presence: true, uniqueness: true, confirmation: true
+  validates :password_confirmation, presence: true
+
   has_many :posts
   has_many :comments, through: :posts
 end
