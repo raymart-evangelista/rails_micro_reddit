a66a704bb3ee6805b8b5a673b3c46df09266c109
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-16 18:43:00 -0700

    migrate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  has_many :child_comments, class_name: "Comment",
+                            foreign_key: "parent_comment_id"
+
+  belongs_to :parent_comment, class_name: "Comment", optional: true
 end
---
+++ db/migrate/20220817013222_add_link_to_posts.rb
@@ -0,0 +1,5 @@
+class AddLinkToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :link, :string
+  end
+end
